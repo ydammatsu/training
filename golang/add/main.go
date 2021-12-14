3157772a6eb3a4f143154fd274c9b1924276e850
ydammatsu <dammatsu.yuta@moneyforward.co.jp>
2021-12-14 22:39:18 +0900

    improve add

--- golang/add/main.go
+++ golang/add/main.go
@@ -1,14 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func add(x, y int) int {
+func add_integer(x, y int) int {
+	return x + y
+}
+
+func add_string(x, y string) string {
 	return x + y
 }
 
 func main() {
-	x := 3
-	y := 4
-	z := add(x, y)
-	fmt.Println(z)
+	x := "1"
+	y := "234"
+	z := add_string(x, y)
+
+	a, err := strconv.Atoi(z)
+	b := 1111
+	if err != nil {
+		fmt.Println("error")
+	} else {
+		c := add_integer(a, b)
+		fmt.Println(c)
+	}
 }
